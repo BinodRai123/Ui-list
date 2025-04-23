f7283c04d7f73fadd7afb39c598a7fb5686bb159
br163406@gmail.com <br163406@gmail.com>
2025-04-22 21:06:17 -1200

    Changing darkmode code to scalable code

--- darkmode.js
+++ darkmode.js
@@ -1,17 +1,33 @@
 const ToggleDiv = document.querySelector(".toogle-darkmode-whitemode");
+const LightMode = ToggleDiv.querySelector(".light-mode");
+const DarkMode = ToggleDiv.querySelector(".dark-mode");
 const Main = document.querySelector("main");
+let count = 0;
 
+const ToggleDetails = [
+    {   icon: LightMode,
+        display: 'block'
+    },
+    {
+        icon: DarkMode,
+        display: 'none'
+    }
+]
 
 ToggleDiv.addEventListener("click", function(e) {
-    let AllIcons = this.querySelectorAll("i");
-    AllIcons.forEach(icon => {
-        if(icon.style.display === 'none') {
-             icon.style.display = 'block';
-             Main.style.backgroundColor = `${icon.getAttribute("aria-valuetext")}`
-             ToggleDiv.parentNode.style.backgroundColor = `${icon.getAttribute("aria-valuetext")}`
-        }
-        else icon.style.display = 'none';
-    });
-
-    AllIcons = null;
+    //Hiding all the icon first
+    ToggleDiv.querySelectorAll("i").forEach(icon => icon.style.display = 'none');
+    
+    //selecting index, icon and color of the icon
+    const Index = count % ToggleDetails.length;
+    const Icon = ToggleDetails[Index].icon;
+    const Color = Icon.getAttribute("aria-valuetext");
+    
+    //Changing BackgroundColor and Icon
+    Icon.style.display = 'block';
+    Main.style.backgroundColor = Color;
+    ToggleDiv.parentNode.style.backgroundColor = Color;
+    
+    //Moving to another icon
+    count++;
 })
